7c6e8c65111a7f05061bf9ff8612643d42299e3b
wshao <wshaozhen@foxmail.com>
2023-07-02 22:04:40 +0800

    init

---
+++ src/test/java/top/liqx/java/study/spring3/Spring3ApplicationTests.java
@@ -0,0 +1,13 @@
+package top.liqx.java.study.spring3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
